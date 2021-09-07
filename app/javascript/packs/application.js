0df0a65f4338ce658413c61ab43f792c31930815
David Colby <davidlcolby@gmail.com>
2021-09-06 22:21:44 -0500

    mrujs added, but not working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
-import "@hotwired/turbo-rails"
 import "channels"
 
-Rails.start()
+import mrujs from "mrujs";
+import CableReady from "cable_ready"
+import { CableCar } from "mrujs/plugins"
+import * as Turbo from "@hotwired/turbo";
+window.Turbo = Turbo;
+
+mrujs.start({
+  plugins: [
+    new CableCar(CableReady)
+  ]
+})
 ActiveStorage.start()
 
 import "controllers"
 import "stylesheets/application.scss"
-
